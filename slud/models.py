6eea215213fd66ecc3761a84401a92618dbd8334
Miguel Rodriguez <marbrb1@gmail.com>
2016-10-10 18:35:47 -0500

    foto/trabajo field blank=True

--- slud/models.py
+++ slud/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 class Speaker(models.Model):
     nombre = models.CharField(max_length=100)
-    trabajo = models.CharField(max_length=100, required=False)
-    foto = models.URLField(max_length=400, required=False)
+    trabajo = models.CharField(max_length=100, blank=True)
+    foto = models.URLField(max_length=400, blank=True)
 
     def __str__(self):
         return self.nombre
